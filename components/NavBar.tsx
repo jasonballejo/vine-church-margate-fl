5702ec919ec0cb7621d6a43895772e997fd0b0f1
Jason Ballejo <ballejo19@gmail.com>
2023-08-30 14:30:30 -0400

    test push4

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function NavBar() {
   return (
     <section>
-      <div>Test Push on Vercel Part 3</div>
+      <div>Test Push on Vercel Part 4</div>
     </section>
   );
 }
