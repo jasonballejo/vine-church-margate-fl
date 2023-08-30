f86b29876cb97bc9ac6f5b24e44e05c6c2f42a9f
Jason Ballejo <ballejo19@gmail.com>
2023-08-30 14:44:40 -0400

    test Vercel push1

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function NavBar() {
   return (
     <section>
-      <div>Test Push on Vercel Part 6</div>
+      <div>
+        Test Push on Vercel Part Waiting to see if Vercel shows the changes
+      </div>
     </section>
   );
 }
