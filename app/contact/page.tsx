bb9fca025c29c41fe30acff6c3f9bb8f30d3b84c
Jason Ballejo <ballejo19@gmail.com>
2023-09-05 16:25:18 -0400

    metadata updated

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact",
+};
+
+export default function Contact() {
+  return <div>page</div>;
+}
