f99700847521af9fdd9dfe08057cdad99aa52272
Jason Ballejo <ballejo19@gmail.com>
2023-09-11 13:50:25 -0400

    update topmenu

--- components/TopMenu.tsx
+++ components/TopMenu.tsx
@@ -4,10 +4,32 @@ import Image from "next/image";
 export default function TopMenu() {
   return (
     // <section className="w-full absolute top-0 -z-10">
-    <section className="w-full h-full relative">
-      <div className="w-full sm:h-[60vh] flex flex-col justify-end items-center relative">
+    <section className="w-full h-full flex justify-center items-center mb-5">
+      <div className="max-w-5xl w-full h-[40vh] flex flex-col justify-end items-center bg-bgTopMenu bg-cover bg-bottom bg-no-repeat border rounded-[16px]">
+        <div className="flex flex-col justify-end gap-5 mb-10 ml-10">
+          <div className="flex flex-col gap-1 text-white">
+            <h2 className="sm:text-4xl text-xl font-bold uppercase z-0">
+              Welcome <span className="font-extralight">home</span>
+            </h2>
+            <p className="sm:w-[70%] w-[85%] sm:text-md text-xs font-extralight tracking-widest z-0">
+              Our burden is to edify a church of overcomers where every member
+              is a minister and every house an extension of the Church thereby
+              conquering our generations to Christ through life-groups that
+              multiply.
+            </p>
+          </div>
+          <div className="flex flex-row gap-5">
+            <button className="sm:w-52 w-20 sm:h-10 h-7 sm:text-lg text-sm uppercase border rounded-md bg-purple-500">
+              Give
+            </button>
+            <button className="sm:w-52 w-36 sm:h-10 h-7 sm:text-lg text-sm text-black uppercase sm:border border-none border-cyan-500 rounded-md bg-white">
+              Join Online
+            </button>
+          </div>
+        </div>
+
         {/* <div className="absolute -z-0 bg-gradient-to-t from-black to-transparent h-[70vh] w-full " /> */}
-        <div className="max-w-5xl w-full flex flex-col justify-end items-center text-white gap-5">
+        {/* <div className="max-w-5xl w-full flex flex-col justify-end items-center text-white gap-5">
           <Image
             className="absolute top-0 sm:w-[1048px] w-[400px] h-[550px] border rounded-[16px] object-cover"
             src="/images/DSC07961.jpg"
@@ -36,7 +58,7 @@ export default function TopMenu() {
               </button>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </section>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,7 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         bgDrawer: "url('/images/DSC00149.jpg')",
+        bgTopMenu: "url('/images/DSC07961.jpg')",
       },
     },
   },
