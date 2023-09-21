c426dd3ab799c7853a8872ecc218c53302e170eb
Jason Ballejo <ballejo19@gmail.com>
2023-09-21 16:25:43 -0400

    comment out

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -13,6 +13,7 @@ export default async function Login() {
   // const session = await getServerSession(authOptions);
 
   // if (session) redirect("/dashboard");
+
   return (
     <>
       <LoginForm />
