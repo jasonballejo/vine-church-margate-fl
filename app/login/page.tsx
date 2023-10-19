ee43dcdeb7e613772fe65415c4da892e086691e1
Jason Ballejo <ballejo19@gmail.com>
2023-10-19 12:13:56 -0400

    update footer calendar link1

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,3 @@
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-// import { authOptions } from "../api/auth/[...nextauth]/route";
-
 import { Metadata } from "next";
 import LoginForm from "@/components/Login-Register/LoginForm";
 
@@ -10,10 +6,6 @@ export const metadata: Metadata = {
 };
 
 export default async function Login() {
-  // const session = await getServerSession(authOptions);
-
-  // if (session) redirect("/dashboard");
-
   return (
     <>
       <LoginForm />
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,12 +1,5 @@
 import RegisterForm from "@/components/Login-Register/RegisterForm";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-// import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function Register() {
-  // const session = await getServerSession(authOptions);
-
-  // if (session) redirect("/dashboard");
-
   return <RegisterForm />;
 }
